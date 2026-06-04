2ef6b6bed509121e22721612708514777f83d21a
Rich Fallat <richfallatjrshop@gmail.com>
2026-06-03 23:30:49 -0700

    Phase 1 WIP

---
+++ Gravitas RotoMotion/Gravitas RotoMotion/Gravitas_RotoMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gravitas_RotoMotionApp.swift
+//  Gravitas RotoMotion
+//
+//  Created by Richard Fallat on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Gravitas_RotoMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gravitas RotoMotion/Gravitas RotoMotionTests/Gravitas_RotoMotionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gravitas_RotoMotionTests.swift
+//  Gravitas RotoMotionTests
+//
+//  Created by Richard Fallat on 6/3/26.
+//
+
+import Testing
+@testable import Gravitas_RotoMotion
+
+struct Gravitas_RotoMotionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
